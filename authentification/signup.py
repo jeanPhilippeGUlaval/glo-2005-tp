74e356055a317e313135420eba0739c3593454d1
jp.gend_mt <jean-philippe.gendreau@momentum-tech.ca>
2024-03-18 20:37:47 -0400

    initial commit

---
+++ authentification/signup.py
@@ -0,0 +1,9 @@
+import pymysql
+import pymysql.cursors
+import random
+import hashlib
+
+def hash_password(password):
+   password_bytes = password.encode('utf-8')
+   hash_object = hashlib.sha256(password_bytes)
+   return hash_object.hexdigest()
---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+import pymysql
+import pymysql.cursors
+import random
+import authentification.signup as auth
+
+app = Flask(__name__)
+conn = pymysql.connect(
+        host='localhost',
+        user='user1',
+        password='A4umTeF4B62*z3c3P*q!j9',
+        db='test' )
+cur = conn.cursor()
+
+@app.route("/")
+def main():
+    return signin()
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/login", methods=['POST'])
+def login():
+    email = '"'+request.form.get('inputEmail')+'"'
+    password = auth.hash_password(request.form.get('inputPassword'))
+    cmd='SELECT password FROM users WHERE email = '+email+';'
+    cur.execute(cmd)
+    passeVrai = cur.fetchone()
+    if (passeVrai!=None) and (password==passeVrai[0]):
+        return render_template('index.html')
+    return main()
+
+
+
+@app.route("/createAccount", methods=['POST'])
+def createAccount():
+    email : str
+    password : str
+    email = '"'+request.form.get('inputEmail')+'"'
+    password = request.form.get('inputPassword')
+    password = '"'+auth.hash_password(password)+'"'
+    id = random.randint(0, 2147483647)
+    print('INSERT INTO users VALUES('+str(id)+', '+email+','+password+');')
+    try:
+        cmd='INSERT INTO users VALUES('+str(id)+', '+email+','+password+');'
+        cur.execute(cmd)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    return signin()
+    
+if __name__ == "__main__":
+    app.run()
+
+cur.close()
+conn.close()
