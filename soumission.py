9391fc22fd26377aed0ad71835fa92102b9df336
jp.gend_mt <jean-philippe.gendreau@momentum-tech.ca>
2024-03-24 18:08:52 -0400

    push

--- soumission.py
+++ soumission.py
@@ -15,21 +15,38 @@ def displaySoumissionID():
 @soumission.route("/soumission/addSoumission", methods=['POST'])
 def addSoumission():
     newSoumissionID = request.form.get('inputSoumissionID')
+    print(newSoumissionID)
     if newSoumissionID.find(" ") != -1:
-        ListOfSoumissions, tableData, headersData, soumissionID = getData(soumissionID)
+        ListOfSoumissions, tableData, headersData, soumissionID = getData("")
         return render_template("soumission.html", lsSoumission=ListOfSoumissions, data = tableData, headers=headersData, soumissionID=soumissionID, error="Il ne peux pas avoir d'espace dans l'identifiant")
     else:
         try:
             cmd = 'INSERT INTO soumission_ids VALUES (\''+ newSoumissionID +'\');'
-            cur=conn.cursor()
             cur.execute(cmd)
+            conn.commit()
         except Exception as e:
             print(e)
+    ListOfSoumissions, tableData, headersData, soumissionID = getData(newSoumissionID)
+    return render_template("soumission.html", lsSoumission=ListOfSoumissions, data = tableData, headers=headersData, soumissionID=soumissionID)
+
+
+@soumission.route("/soumission/supprimerSoumission", methods=['GET'])
+def deleteSoumission():
+    SoumissionID = request.args.get('id')
+    try:
+        cmd = 'DELETE FROM soumission_ids WHERE ID = \''+ SoumissionID +'\';'
+        cur.execute(cmd)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    ListOfSoumissions, tableData, headersData, soumissionID = getData("")
+    return render_template("soumission.html", lsSoumission=ListOfSoumissions, data = tableData, headers=headersData, soumissionID=soumissionID)
+
 
 def getData(soumissionID):
     ListOfSoumissions = getSoumissionList()
     headersData = getHeaders("soumission")
-    if soumissionID == None:
+    if soumissionID == None or soumissionID == "":
         cmd = 'SELECT * FROM soumission;'
         soumissionID = "Toutes les Soumissions"
     else:
